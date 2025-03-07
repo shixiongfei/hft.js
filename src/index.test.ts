33ce41fb1809127365395c7c5425e236dc4c7270
shixiongfei <xiongfei.shi@icloud.com>
2025-03-07 23:06:57 +0800

    更换测试品种

--- src/index.test.ts
+++ src/index.test.ts
@@ -46,7 +46,7 @@ if (!existsFile(config.FlowMdPath)) {
 class Strategy implements hft.IStrategy, hft.ITickReceiver {
   private lastTick?: hft.TickData;
   private engine: hft.IRuntimeEngine;
-  readonly symbol = "sc2504.INE";
+  readonly symbol = "ni2505.SHFE";
 
   constructor(engine: hft.IRuntimeEngine) {
     this.engine = engine;
